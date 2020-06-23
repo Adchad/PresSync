a068aec4ca36aa1024289bc0f2870151f1a8f5a2
Adam Chader <adam.chader@telecom-paris.fr>
2020-06-23 13:34:37 +0200

    erreur réglée je crois

---
+++ firstserverclone/hello_flask.py
@@ -0,0 +1,33 @@
+
+import flask
+
+from flask_socketio import SocketIO, send
+
+APP = flask.Flask(__name__)
+socketio = SocketIO(APP)
+
+@APP.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+
+#@APP.route('/hello/<name>/')
+#def hello(name):
+
+ #     return flask.render_template('hello.html', name=name)
+
+
+   
+@socketio.on('message')
+def handle_message(message):
+        print('message reçu: ' + message )
+        send("Voici le message de retour")
+
+
+if __name__ == '__main__':
+    APP.debug= True
+    socketio.run(APP)
+
+
+
